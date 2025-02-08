d981032eb51783a5f2ba667dbf17c190a3b5a018
Swagat Kumar Mahapatra <swagatmahapatra2004@gmail.com>
2025-02-08 12:30:38 +0530

    GlueStack initialization

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { View,Text } from "react-native";
 export default function homescreen(){
     return(
-        <View>
-            <Text style = {{fontSize: 30}}>Home Screen</Text>
-        </View>
+        <Button><Text>cliick</Text></Button>
     );
 } 
