5fb96174101c946f2f4a06d939da58b4dad83f95
Swagat Kumar Mahapatra <swagatmahapatra2004@gmail.com>
2025-02-09 03:42:51 +0530

    ui starts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import {Slot, Stack, Tabs } from "expo-router";
 
-import "@/global.css";
+import "../global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout(){
     
-    return <GluestackUIProvider mode="light"><Tabs/></GluestackUIProvider>;
+    return <GluestackUIProvider>
+        <Stack/>
+    </GluestackUIProvider>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,40 @@
 import { Button } from "@/components/ui/button";
-import { View,Text } from "react-native";
-export default function homescreen(){
+import {
+    Avatar,
+    AvatarBadge,
+    AvatarFallbackText,
+    AvatarImage,
+  } from '@/components/ui/avatar';
+  import { View, Text,Image, StyleSheet } from "react-native";
+  import vector from "../assets/images/vector.png";
+
+export default function avatar(){
     return(
-        <Button><Text>cliick</Text></Button>
-    );
-} 
+        <View style={styles.container}>
+            <Image source={vector} style={styles.Image} />
+        <Avatar  size="lg">
+          <AvatarFallbackText />
+          <AvatarImage source={{
+          uri:"https://api.dicebear.com/9.x/avataaars-neutral/svg"}} />
+            
+          <AvatarBadge />
+        </Avatar>
+        </View>
+        );
+    }
+      const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            position: 'absolute',
+            justifyContent: 'flex-start',
+            alignItems: 'flex-end',
+            paddingTop: 50,
+            paddingBottom: 500,
+            paddingRight: 20,
+        },
+        Image: {
+            width: '100%',
+            height: '100%',
+            marginTop:50,
+        }
+    });
