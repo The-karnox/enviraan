cc73562e0132f12fc4fddf361e44c28f52b42410
Swagat Kumar Mahapatra <swagatmahapatra2004@gmail.com>
2025-02-08 09:01:03 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import { View,Text } from "react-native";
+export default function homescreen(){
+    return(
+        <View>
+            <Text style = {{fontSize: 30}}>Home Screen</Text>
+        </View>
+    );
+} 
